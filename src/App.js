8d610bd2ca2e9c0b9cfc12685d875393074c6c43
ravalidesign <ravalidesign@gmail.com>
2021-04-21 11:38:05 -0500

    deletefunction-styles

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import './App.css';
 import React from "react"
-import {Modal} from "./Components/Content/Modal"
-import {Print} from "./Components/Content/Print"
-import {Navbar} from "./Components/Content/Navbar"
+import { Modal } from "./Components/Content/Modal"
+import { Print } from "./Components/Content/Print"
+import { Navbar } from "./Components/Content/Navbar"
 import crearnota from './assets/crearnota.png';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-     <Modal />
-    <button className="createNote"><a href="#miModal"> <img src={crearnota} alt="crearnota"/></a></button>
-    <div className="impNotes">
-       <h2>Historial de tus notas </h2>
-       <Print />
-      </div>
-    
+      <Navbar />
       
+      <Modal />
+     
+      <button className="createNote"><a href="#miModal"> <img src={crearnota} alt="crearnota" /></a></button>
+     
+      
+      <div className="impNotes">
+        <h2>Historial de tus notas </h2>
+        <Print />
+      </div>
+
+
 
 
 
--- src/Components/Content/Print.js
+++ src/Components/Content/Print.js
@@ -4,59 +4,76 @@ import { fireStore } from "../../firebase";
 
 export const Print = () => {
     const [notes, setNotes] = useState([])
-   
+    
+
     useEffect(() => {
 
         const getNotes = async () => {
             const { docs } = await fireStore.collection("remember").get()
-            const newArray = docs.map ( item => ( { id: item.id, ...item.data() } ))
+            const newArray = docs.map(item => ({ id: item.id, ...item.data() }))
             setNotes(newArray)
         }
 
         getNotes()
     }, []);
 
- 
+
     const deleteNote = async (id) => {
-        try{
-        await fireStore.collection("remember").doc(id).delete()
-        const { docs } = await fireStore.collection("remember").get()
-        const newArray = docs.map(item => ({ id: item.id, ...item.data() }))
-        setNotes(newArray)
-        }catch(e) {
+        try {
+            await fireStore.collection("remember").doc(id).delete()
+            const { docs } = await fireStore.collection("remember").get()
+            const newArray = docs.map(item => ({ id: item.id, ...item.data() }))
+            setNotes(newArray)
+        } catch (e) {
             console.log(e)
         }
-    } 
+    }
+
+
+    const updateNote = async (id) => {
+        try {
+            const dt = await fireStore.collection("remember").doc(id).get();
+            console.log(dt.data())
+        } catch (e) {
+            console.log(e)
+        }
+
+
+    }
 
     return (
-    <>
-        <div className="Print">
+        <>
+            <div >
 
-            <div>
-                {
-                    notes.length !== 0 ? (
-                        notes.map(item => (
-                            <div className="printNotes" key={item.id}>
-                             <h4>{item.date}</h4>
-                             <h4>{item.title}</h4>
-                             <h4>{item.description}</h4>
-                            <button onClick={(id) => {deleteNote (item.id) }}>Borrar</button>
-                            </div>
-                        ))
-
-
-                    ) : (<span> lo siento no hay notas </span>)
-                }
-
+                <div className="Print">
+                    {
+                        notes.length !== 0 ? (
+                            notes.map(item => (
+                                <div className="printNotes" key={item.id}>
+                                    <div className='content'>
+                                    <h4>{item.date}</h4>
+                                    <h4>{item.title}</h4>
+                                    <h4>{item.description}</h4>
+                                    <button onClick={(id) => { deleteNote(item.id) }}>Borrar</button>
+                                    <button onClick={(id) => { updateNote(item.id) }}><a href="#miModal"></a>Actualizar</button>
+                                   </div>
+                                </div>
+                            ))
 
 
+                        ) : (<span> lo siento no hay notas </span>)
+                    }
 
 
 
+
+
+
+                </div>
+               
             </div>
-        </div>
 
         </>
     )
-  
+
 }
