7dfef9b9b3f4e3d9c260348dd1af3557766fb025
ravalidesign <ravalidesign@gmail.com>
2021-04-22 22:00:00 -0500

    router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import './App.css';
 import React from "react"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+
 import { Modal } from "./Components/Content/Modal"
 import { Print } from "./Components/Content/Print"
 import { Navbar } from "./Components/Content/Navbar"
@@ -7,25 +14,28 @@ import crearnota from './assets/crearnota.png';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      
-      <Modal />
-     
-      <button className="createNote"><a href="#miModal"> <img src={crearnota} alt="crearnota" /></a></button>
-     
-      
-      <div className="impNotes">
-        <h2>Historial de tus notas </h2>
-        <Print />
-      </div>
+    <Router>
+      <Switch>
 
+        <Route path="/wall">
+          <div className="App">
 
+            <Navbar />
+            <Modal />
+            <button className="createNote"><a href="#miModal"> <img src={crearnota} alt="crearnota" /></a></button>
+            <div className="impNotes">
+              <h2>Historial de tus notas </h2>
+              <Print />
+            </div>
+          </div>
+        </Route>
 
+        <Route path="/">
+          Estas en inicio 
+        </Route>
+      </Switch>
 
-
-
-    </div>
+    </Router>
   );
 }
 
